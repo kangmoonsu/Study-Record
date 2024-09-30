534316bb10fc71f9d069e5ca7788ebdbc6e29338
강문수 <moonsu1011@naver.com>
2024-09-30 10:54:15 -0500

    assign Lab2

--- C++/Lab assignment/Lab-2.cpp
+++ C++/Lab assignment/Lab-2.cpp
@@ -2,8 +2,41 @@
 using namespace std;
 
 int main() {
+    double money;
+    cout << "Enter floating point value: ";
+    cin >> money;
+    
+    // Convert dollars to cents.(double into int)
+    int cents = money * 100;
 
+    int ten_dollars = cents / 1000;
+    cents %= 1000;
 
+    int five_dollars = cents / 500;
+    cents %= 500;
+
+    int one_dollar = cents / 100;
+    cents %= 100;
+
+    int quarters = cents / 25;
+    cents %= 25;
+
+    int dimes = cents / 10;
+    cents %= 10;
+
+    int nickels = cents / 5;
+    cents %= 5;
+
+    int pennies = cents;
+
+    // Result
+    cout << ten_dollars << " ten-dollar bill(s)" << endl;
+    cout << five_dollars << " five-dollar bill(s)" << endl;
+    cout << one_dollar << " one-dollar bill(s)" << endl;
+    cout << quarters << " quarter(s)" << endl;
+    cout << dimes << " dime(s)" << endl;
+    cout << nickels << " nickel(s)" << endl;
+    cout << pennies << " pennies(s)" << endl;
 
     return 0;
 }
