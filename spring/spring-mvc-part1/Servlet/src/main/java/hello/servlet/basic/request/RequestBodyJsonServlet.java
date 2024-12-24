1e453757f11108112b2b927448419a8d09bc7d4b
Michael <moonsu1011@naver.com>
2024-12-24 15:41:39 -0600

    spring mvc structure basic

--- spring/spring-mvc-part1/Servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
+++ spring/spring-mvc-part1/Servlet/src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -25,6 +25,11 @@ public class RequestBodyJsonServlet extends HttpServlet {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
+    protected long getLastModified(HttpServletRequest req) {
+        return super.getLastModified(req);
+    }
+
+    @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ServletInputStream inputStream = request.getInputStream();
         String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
--- spring/spring-mvc-part1/Servlet/src/main/webapp/jsp/members/new-form.jsp
+++ spring/spring-mvc-part1/Servlet/src/main/webapp/jsp/members/new-form.jsp
@@ -5,8 +5,8 @@
 </head>
 <body>
 <form action="/jsp/members/save.jsp" method="post">
-    username: <input type="text" name="username" />
-    age: <input type="text" name="age" />
+    username: <input type="text" name="username"/>
+    age: <input type="text" name="age"/>
     <button type="submit">submit</button>
 </form>
 </body>
